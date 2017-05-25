8012941129145817d264ae7d177966246a574dc9
Nicholas Raethke and Qudsia Ahmad <nicholas@>
2017-05-24 21:12:49 -0700

    fix list image issue in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
   $("button#kitty").click(function() {
-    $("ul#cat").after("<li>Meow!</li>").append("<li><img src='img/puppy1.jpg'></li>");
-    $("ul#dog").after("<li>Woof woof!</li>").append("<li><img src='img/kitty1.jpeg'></li>");
+    $("ul#cat").append("<li>Meow! <img src='img/puppy1.jpg' id='puppy1'></li>");
+    $("ul#dog").append("<li>Woof woof! <img src='img/kitty1.jpeg' id='kitty1'</li>");
     $('li').click(function() {
-      alert('hi');
+
+      $("ul#cat").children('li').first().click(function() {
+        $(this).remove();
+      });
+      $("ul#dog").children('li').first().click(function() {
+        $(this).remove();
+      });
     });
   });
 
   $("button#puppy").click(function() {
-    $("ul#cat").after("<li>reeeoow!</li>").append("<li><img src='img/kitty2.jpeg'></li>");
-    $("ul#dog").before("<li>rwah!</li>").append("<li><img src='img/puppy2.jpg'></li>");
+    $("ul#cat").append("<li>reeeoow! <img src='img/kitty2.jpeg' id='kitty2'></li>");
+    $("ul#dog").append("<li>rwah! <img src='img/puppy2.jpg' id='puppy2'></li>");
+
+    $("ul#cat").children('li').first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children('li').first().click(function() {
+      $(this).remove();
+    });
   });
 });
